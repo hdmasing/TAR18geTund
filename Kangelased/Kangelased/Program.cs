6c2f974822b939eedbbe888aa4d4219eb8915a0f
hdmasing <hansdietrich.masing@gmail.com>
2019-05-23 18:04:13 +0300

    Main valmis

--- Kangelased/Kangelased/Program.cs
+++ Kangelased/Kangelased/Program.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO
 
 namespace Kangelased
 {
     class Program
     {
+        static List<Kangelane> kangelased = new List<Kangelane>();
+        static void Loefailist(string failinimi)
+        {
+            string[] failiandmed = File.ReadAllLines(failinimi);
+            foreach (string rida in failiandmed)
+            {
+                string[] nimi_ja_koht = rida.Split('/');
+                for (int i = 0; 1 < nimi_ja_koht.Length; i++)
+                {
+                    nimi_ja_koht[i] = nimi_ja_koht[i].Trim();
+                }
+                if (nimi_ja_koht[0].EndsWith("*")){
+                    kangelased.Add(new SuperKangelane(nimi_ja_koht[0], nimi_ja_koht[1]));
+                }
+                else { kangelased.Add(new Kangelane(nimi_ja_koht[0], nimi_ja_koht[1]));
+                }
+            }
+        }
         static void Main(string[] args)
         {
             
